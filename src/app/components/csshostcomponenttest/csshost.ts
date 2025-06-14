b959bb96c78a56ee4ea1373ec741072a0f4a2b64
Baltazar <baltazarottonello@gmail.com>
2025-06-14 12:59:21 -0300

    first commit

---
+++ src/app/components/csshostcomponenttest/csshost.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+
+@Component({
+  selector: "app-csshost",
+  imports: [],
+  templateUrl: "./csshost.html",
+  styleUrl: "./csshost.css",
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CssHostComponentTest {}
